b84dddc5e7db003db0f8b64b3aaab5e5f20e2a44
Jhonatan <jhonatanscremim@gmail.com>
2023-04-18 11:24:30 -0300

    feat(api-gateway)
    Cria API Gateway utilizando Ocelot

---
+++ src/BooksShop.APIGateway/BooksShop.APIGateway.API/Program.cs
@@ -0,0 +1,26 @@
+using Ocelot.Middleware;
+using Ocelot.DependencyInjection;
+using System.Net;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("Ocelot.json");
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseOcelot().Wait();
+
+app.Run();
